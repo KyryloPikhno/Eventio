1ac6cfddcd21619e424130bea71b5554416092ef
KyryloPikhno <Mr.Good@i.ua>
2025-02-23 18:53:11 +0100

    Fix forgot password forms

--- src/app/auth/components/ForgotPasswordForm.tsx
+++ src/app/auth/components/ForgotPasswordForm.tsx
@@ -1,45 +1,61 @@
 "use client"
 
 import forgotPassword from "@/app/auth/mutations/forgotPassword"
-import { ForgotPassword } from "@/app/auth/validations"
-import { Form, FORM_ERROR } from "@/app/components/Form"
-import LabeledTextField from "@/app/components/LabeledTextField"
+import { FORM_ERROR } from "@/app/components/Form"
 import { useMutation } from "@blitzjs/rpc"
+import { Button, Stack, Text, TextInput, Title } from "@mantine/core"
+import { useForm } from "@mantine/form"
 
 export function ForgotPasswordForm() {
   const [forgotPasswordMutation, { isSuccess }] = useMutation(forgotPassword)
 
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  })
+
+  const handleSubmit = async (values: any) => {
+    try {
+      await forgotPasswordMutation(values)
+    } catch (error: any) {
+      return {
+        [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
+      }
+    }
+  }
+
   return (
-    <>
-      <h1>Forgot your password?</h1>
+    <Stack>
+      <Title>Forgot your password?</Title>
       <>
         {isSuccess ? (
-          <div>
-            <h2>Request Submitted</h2>
-            <p>
+          <Stack>
+            <Title>Request Submitted</Title>
+            <Text>
               If your email is in our system, you will receive instructions to reset your password
               shortly.
-            </p>
-          </div>
+            </Text>
+          </Stack>
         ) : (
-          <Form
-            submitText="Send Reset Password Instructions"
-            schema={ForgotPassword}
-            initialValues={{ email: "" }}
-            onSubmit={async (values) => {
-              try {
-                await forgotPasswordMutation(values)
-              } catch (error: any) {
-                return {
-                  [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
-                }
-              }
-            }}
-          >
-            <LabeledTextField name="email" label="Email" placeholder="Email" />
-          </Form>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <TextInput
+              withAsterisk
+              label="Email"
+              placeholder="your@email.com"
+              key={form.key("email")}
+              {...form.getInputProps("email")}
+            />
+
+            <Button type="submit">Send Reset Password Instructions</Button>
+          </form>
         )}
       </>
-    </>
+    </Stack>
   )
 }
