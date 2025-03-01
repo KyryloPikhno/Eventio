a06c5cb22558fa590ad6f6f862b26bd3e0146fb1
KyryloPikhno <Mr.Good@i.ua>
2025-03-01 21:25:59 +0100

    Fix login screen

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,4 +1,4 @@
-import {ForgotPasswordForm} from "../components/ForgotPasswordForm"
+import { ForgotPasswordForm } from "@/app/auth/components/ForgotPasswordForm"
 
 export default function ForgotPasswordPage() {
   return <ForgotPasswordForm />
